990584f5831195b0cbd1179f8a67bb93da198ceb
Rdowafun <jabrooster@gmail.com>
2023-02-05 00:06:22 +0500

    feat: add answer

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.middle = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        this.middle = Math.round((this.max - this.min) / 2) + this.min;
+        return this.middle
     }
 
     lower() {
-
+        this.max = this.guess()
+        return this.max
     }
 
     greater() {
-
+        this.min = this.guess()
+        return this.min
     }
 }
 
